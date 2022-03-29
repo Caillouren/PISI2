deb1a3db6d309d253393d01611bbaa0ffadb07ec
Caillouren <caio_filipe064@hotmail.com>
2022-03-28 23:52:58 -0300

    Código geral das últimas semanas
    Toda a codificação resultando das últimas semanas sendo compilada de uma vez.

--- Projeto/Projeto.py
+++ Projeto/Projeto.py
@@ -1,11 +1,44 @@
-def PrintMatriz(matriz):
-    linhas = len(matriz)
-    colunas = len(matriz[0])
+# Importe da função permutations
+from itertools import permutations
+
+# Criação da Matriz e leitura do .txt contendo a matriz
+Matriz = []
+Matriz_Entrada = open('/Users/Caio_/Documents/GitHub/PISI2/Projeto\MatrizEntrada.txt', 'r')
+
+# Quantidade de linhas e colunas da matriz
+QtdLinhas = int(Matriz_Entrada.readline(1))
+QtdColunas = int(Matriz_Entrada.readline())
+
+# Mapeando a Matriz
+for i in Matriz_Entrada:
+    Matriz_Entrada
+    if i != "":
+        Matriz.append(i.split())
+
+# Criando o Print da Matriz e o realizando
+linhas = len(Matriz)
+colunas = len(Matriz[0])
+def PrintMatriz(Matriz):
     for l in range(linhas):
         linha = ""
         for c in range(colunas):
-            linha = linha + " " + str(matriz[l][c])
+            linha = linha + " " + str(Matriz[l][c])
         print(linha)
 
-matriz = [[1,2,3],[4,5,6]]
-PrintMatriz(matriz)
+PrintMatriz(Matriz)
+
+# Localizando os pontos da Matriz
+pontos = {}
+for l in range(linhas):
+    for c in range(colunas):
+        if Matriz[l][c] == 'R':
+            pontos['RFinal'] = [l, c]
+        if Matriz[l][c] != '0':
+            pontos[Matriz[l][c]] = [l, c]
+
+# Calculando a distância entre os pontos
+def distancias(Coord1, Coord2):
+    return abs((int(Coord1[0]) - int(Coord2[0]))) + ((int(Coord1[1]) - int(Coord2[1])))
+
+Recebidos = permutations(pontos)
+print(Recebidos)
